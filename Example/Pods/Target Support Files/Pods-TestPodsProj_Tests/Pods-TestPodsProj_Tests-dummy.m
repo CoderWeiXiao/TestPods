19582a06243a6bed6e4a8bfba100a504bb4248b3
weixiao <lougeweixiao@163.com>
2020-08-10 10:58:08 +0800

    remove example

--- Example/Pods/Target Support Files/Pods-TestPodsProj_Tests/Pods-TestPodsProj_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestPodsProj_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TestPodsProj_Tests
-@end
--- Example/Pods/Target Support Files/TestPodsProj/TestPodsProj-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TestPodsProj : NSObject
-@end
-@implementation PodsDummy_TestPodsProj
-@end
